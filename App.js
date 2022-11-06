77a7da5d77798a632d88d41ca09ea9cafc604d3d
BeepingJohnson <jdfranco@asu.edu>
2022-11-05 22:08:42 -0700

    intro

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image style={styles.logo}
+        source={require('./assets/InTune_Logo.png')}
+        />
+      <Text style={styles.title}>
+      Share, Listen, Together
+      </Text>
     </View>
   );
 }
@@ -13,8 +16,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0E0E0E',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  loadingscreen:{
+    alignItems: 'flex-start',
+    backgroundColor: '#0E0E0E',
+    borderWidth: 1,
+    borderRadius: 32,
+    gap: 98,
+    flexDirection: "column",
+    height: 844,
+    padding: 234 ,
+    position: "relative",
+    width: 390,
+    display: 'flex',
+    overflow: 'hidden',
+  },
+  title:{
+    color: '#ce6ef2',
+    fontSize: 24,
+    fontWeight: '400',
+    letterSpacing: 0,
+    textAlign: 'center',
+    width: 390,
+  },
+  logo:{
+    height: 129,
+    width: 315,
+    border: 1,
+    padding: 20,
+    paddingVertical: 40,
+    top: -50,
+    bottom: '45%'
+  }
 });
