8ac1d81f19cbd83019cd2c9f8b507e91251129e4
Hector Durazo <hectordurazo1@gmail.com>
2022-11-06 20:25:13 -0700

    not sure what changed lol

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
+import React from "react";
+import { StyleSheet, View, Text, Image, ImageBackground } from "react-native";
+import Intro from "./pages/Intro";
+import LoginScreen from "./pages/LoginScreen";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
